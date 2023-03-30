199a3f83b25582490c9cb8e1a35eb8bcc2607a9b
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-31 00:27:04 +0530

    leartning

---
+++ React/Lession03/Practice/index.js
@@ -0,0 +1,26 @@
+// let show = false;
+
+// const handleClick = () => {
+//   show = !show;
+//   root.render(<App />);
+// };
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Coding Ninjas</h1>
+//       <button onClick={handleClick}>
+//         {show ? "Hide button" : "show button"}
+//       </button>
+//       {show && <p>Hello</p>}
+//     </>
+//   );
+// }
+
+// DYNAMIC FORM SUBMIT:
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
