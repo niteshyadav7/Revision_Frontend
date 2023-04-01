48337fbb70f28661cba2c4b65a849de5c39cee48
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-04-01 11:52:06 +0530

    learning

---
+++ DSA/Array/prob2d01.java
@@ -0,0 +1,23 @@
+package DSA.Array;
+
+import java.util.Scanner;
+
+public class prob2d01 {
+    public static void main(String[] args) {
+       Scanner scn =new Scanner(System.in);
+       int n=scn.nextInt(); 
+       int m=scn.nextInt();
+       int [][]arr=new int [n][m];
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            arr[i][j]=scn.nextInt();
+        }
+       }
+       for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+       }
+    }
+}
--- React/lession04/src/MovieCard.js
+++ React/lession04/src/MovieCard.js
@@ -1,15 +1,57 @@
 import React from "react";
 
 class MovieCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "The Avenger",
+      plot: "Supernatural power shown in movie",
+      price: 199,
+      rating: 8.9,
+    };
+   
+    }
+     handlePlus=()=>{
+      console.log(`stars :`,this);
+  }
   render() {
+    const {title,plot,price,rating}=this.state;
     return (
       <>
         <img
           src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
-          alt="images" width={100}
+          alt="images"
+          width={100}
         />
-        <h3>The Avenger</h3>
-        <p></p>
+        {/* <h3>{this.state.title}</h3>
+        <p>{this.state.plot}</p>
+        <p>Rs.{this.state.price}</p>
+        <p>{this.rating}</p> */}
+        <h3>{title}</h3>
+        <p>{plot}</p>
+        <p>{price}</p>
+        <p>{rating}</p>
+        <div>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/43/43625.png"
+            alt="images of minus"
+            width={10}
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
+            alt="images of star"
+            width={25}
+          />
+          <img onClick={this.handlePlus}
+            src="https://cdn-icons-png.flaticon.com/512/748/748113.png"
+            alt="images of plus"
+            width={10}
+          />
+        </div>
+        <p>
+          <button>favorite</button>&nbsp;
+          <button>Add to cart</button>
+        </p>
       </>
     );
   }
