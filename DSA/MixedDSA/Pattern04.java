8feb5a2d1ea12bf1d0c822b2b0b6679a9482a3bf
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-05-16 11:41:42 +0530

    dst

--- DSA/MixedDSA/Pattern04.java
+++ DSA/MixedDSA/Pattern04.java
@@ -3,11 +3,11 @@ package DSA.MixedDSA;
 import java.util.Scanner;
 
 public class Pattern04 {
-//      *
-//     **
-//    ***
-//   ****
-//  *****
+    // *
+    // **
+    // ***
+    // ****
+    // *****
     public static void spacesPattern(int n) {
         for (int i = 1; i <= n; i++) {
             for (int k = 1; k <= n - i; k++) {
@@ -20,9 +20,32 @@ public class Pattern04 {
         }
     }
 
+    
+    // IMPORTANT QUESTION:
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+    public static void trianglePattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        spacesPattern(num);
+        // spacesPattern(num);
+        trianglePattern(num);
     }
 }
